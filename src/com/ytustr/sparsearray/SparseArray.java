02966569bc45af8388b1371f5535c64541152d2b
Ytu-str <1466779249@qq.com>
2022-10-11 21:51:11 +0800

    稀疏数组的写入与读取

--- src/com/ytustr/sparsearray/SparseArray.java
+++ src/com/ytustr/sparsearray/SparseArray.java
@@ -1,12 +1,16 @@
 package com.ytustr.sparsearray;
 
+import jdk.internal.util.xml.impl.Input;
+
+import java.io.*;
+
 /**
  * 稀疏数组
  *
  * @author Ytu-str
  */
 public class SparseArray {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         int[][] chessArr = new int[11][11];
         chessArr[1][2] = 1;
         chessArr[2][3] = 2;
@@ -52,6 +56,7 @@ public class SparseArray {
             }
             System.out.println();
         }
+
         //恢复为二维数组
         int[][] chessArray2 = new int[sparseArray[0][0]][sparseArray[0][1]];
         for (int i = 1; i < sparseArray.length; i++) {
@@ -63,5 +68,38 @@ public class SparseArray {
             }
             System.out.println();
         }
+
+        //二维数组写入文件
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(new File("D:/map.data.txt")),"utf-8"));
+        for (int i = 0; i < sparseArray.length; i++) {
+            for (int j = 0; j < sparseArray.length; j++) {
+                bufferedWriter.write(sparseArray[i][j]+" ");
+            }
+            bufferedWriter.newLine();
+        }
+        bufferedWriter.close();
+        //读取文件中的稀疏数组
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(new File("D:/map.data.txt"))));
+        String line = null;
+        int[][] chess3 = new int[sparseArray.length][sparseArray.length];
+        int num = 0;
+        while ((line = bufferedReader.readLine()) != null){
+            String[] split = line.split(" ");
+            for (int i = 0; i < split.length; i++) {
+                chess3[num][i] = Integer.parseInt(split[i]);
+            }
+            num ++;
+        }
+        bufferedReader.close();
+
+        System.out.println("打印文件读取稀疏数组：");
+        for (int[] row : chess3) {
+            for (int item : row) {
+                System.out.printf("%d\t",item);
+            }
+            System.out.println();
+        }
     }
+
+
 }
